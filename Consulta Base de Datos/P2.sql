0f63d3496b41bfdca676fb571094a5fe1c3cd270
Faaian <134747814+Faaian@users.noreply.github.com>
2023-08-12 11:17:44 -0400

    Add files via upload

---
+++ Consulta Base de Datos/P2.sql
@@ -0,0 +1,27 @@
+SELECT
+    nombre_emp,
+    appaterno_emp
+FROM empleado
+WHERE
+    nombre_emp LIKE '%C%'
+;
+
+SELECT
+    fecini_arriendo,
+    --EXTRACT(DAY FROM fecini_arriendo),
+    --EXTRACT(MONTH FROM fecini_arriendo),
+    --EXTRACT(YEAR FROM fecini_arriendo) AS "AÑO",
+    TO_CHAR(fecini_arriendo, 'dd/mm/yyyy') AS "FECHA INCIO ARRIENDO"
+    --SYSDATE
+FROM propiedad_arrendada
+WHERE
+    --EXTRACT(YEAR FROM fecini_arriendo) = EXTRACT(YEAR FROM SYSDATE)
+    EXTRACT(YEAR FROM fecini_arriendo) = 2023
+;
+
+SELECT
+    fecini_arriendo
+FROM propiedad_arrendada
+--WHERE
+    --EXTRACT (YEAR FROM fecini_arriendo) = 19
+;
