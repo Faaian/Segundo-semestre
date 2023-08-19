e33b1370c4b6b5184b528e1451be96934cb68e7d
Fabian <fabi.loyola@duocuc.cl>
2023-08-19 09:55:24 -0400

    Nueva Clase

---
+++ Desarrollo de Software de Escritorio/Fabian/SpotifyTrucho/src/main/java/cl/duoc/pgy2121/spotifytrucho/SpotifyTrucho.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cl.duoc.pgy2121.spotifytrucho;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class SpotifyTrucho {
+    private String nombre = "Fabian";
+    
+    public static void main(String[] args) {
+        //Variables
+        char sexo = 'M';
+            String nombre = "Fabian";
+        boolean isValid = false;
+            byte bytes;  
+        int edad = 18;
+            float decimal = 18.5f;
+            double doble = 3.10d;
+            long numeroLargo = 2323232323232l;
+            short numeroChico = 34;
+            
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Edad: "+ edad);
+        System.out.println("Activo: "+ isValid);
+    }
+    
+}
+
