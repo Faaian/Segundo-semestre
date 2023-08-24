71908a144255898d877cae3209f0c9bcd9a9d920
Faaian <fabi.loyola@duocuc.cl>
2023-08-24 02:30:13 -0400

    caso 3

--- Consulta Base de Datos/Todos los P/p3.sql
+++ Consulta Base de Datos/Todos los P/p3.sql
@@ -22,4 +22,16 @@ ORDER BY
 ;
 
 -- Caso 3
+SELECT
+    numrun_emp || ' ' || dvrun_emp AS "RUN EMPLEADO",
+    pnombre_emp || ' ' || snombre_emp || ' ' || appaterno_emp || ' ' || apmaterno_emp AS "NOMBRE COMPLETO",
+    sueldo_base AS "SUELDO BASE",
+    TO_CHAR(fecha_nac, 'DD/MM/YYYY') AS "FECHA DE NACIMIENTO",
+    SUBSTR(pnombre_emp,1,3) ||LENGTH(pnombre_emp)|| '*' || SUBSTR(sueldo_base,-1,1)||dvrun_emp||ROUND(EXTRACT(YEAR FROM SYSDATE)-EXTRACT(YEAR FROM fecha_contrato)) AS "NOMBRE DE USUARIO",
+    SUBSTR(numrun_emp,3,1)|| EXTRACT(YEAR FROM fecha_nac) + 2||SUBSTR(sueldo_base,-3,3) - 1||SUBSTR(appaterno_emp,-2,2)|| EXTRACT(MONTH FROM SYSDATE) AS "CLAVE"
+FROM empleado
+ORDER BY appaterno_emp
+;
+
+-- Caso 3
 SELECT * FROM empleado;
