bdb3b6a3d9446ff68ab3abd3b6dab5c67e9bfd4d
Fabian <fabi.loyola@duocuc.cl>
2023-08-24 20:48:47 -0400

    java

---
+++ Desarrollo de Software de Escritorio/Fabian/Pizzeria/src/pizzeria/Pizzeria.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pizzeria;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class Pizzeria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int opcion;
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Bienvenido a Pizzeria");
+        System.out.println("1) Pizza Napolitana");
+        System.out.println("2) Pizza Peperoni");
+        System.out.println("3) Pizza Hawaiana");
+        System.out.println("4) Cancelar pedido");
+        System.out.println("5) Finalizar compra");
+        System.out.println("0) Salir");
+        System.out.println("Seleccionar opcion:");
+    }
+    
+}
