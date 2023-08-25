e1914077ac49d3582349143132d8987bfdae5af1
Fabian <fabi.loyola@duocuc.cl>
2023-08-24 22:16:24 -0400

    Clase terminada

--- Desarrollo de Software de Escritorio/Fabian/Pizzeria/src/pizzeria/Pizzeria.java
+++ Desarrollo de Software de Escritorio/Fabian/Pizzeria/src/pizzeria/Pizzeria.java
@@ -19,13 +19,16 @@ public class Pizzeria {
         // TODO code application logic here
         int opcion;
         Scanner teclado = new Scanner(System.in);
+        Pizza pizza1 = new Pizza();
+        Pizza pizza2 = new Pizza("Peperoni", "XL", "Normal");
+        
         do {
             System.out.println("Bienvenido a Pizzeria");
             System.out.println("1) Pizza Napolitana");
             System.out.println("2) Pizza Peperoni");
             System.out.println("3) Pizza Hawaiana");
             System.out.println("4) Cancelar pedido");
-            System.out.println("5) Finalizar compra");
+            System.out.println("5) Resumen compra");
             System.out.println("0) Salir");
             System.out.println("Seleccionar opcion:");
             opcion = teclado.nextInt();
@@ -33,16 +36,50 @@ public class Pizzeria {
             switch(opcion){
                 /*case 1:
                     System.out.println("Solicito Pizza napolitana");
-                    break;*/
-                case 1 -> System.out.println("Solicito Pizza napolitana");
-                case 2 -> System.out.println("Solicito Pizza peperoni");
-                case 3 -> System.out.println("Solicito Pizza hawaiana");
-                case 4 -> System.out.println("Pedido cancelado");
-                case 5 -> System.out.println("Resumen compra");
-                case 0 -> System.out.println("Adios");
-                default -> System.out.println("Opcion incorrecta");
+                    break;
+                */
+                case 1 -> {
+                    System.out.println("Solicito Pizza napolitana");
+                    pizza1.setNombre("Napolitana");
+                    System.out.println("Ingrese tipo de masa:");
+                    pizza1.setMasa(teclado.next());
+                    System.out.println("Ingrese el tamano:");
+                    pizza1.setTamano(teclado.next());
+                }
+                case 2 -> {
+                    System.out.println("Solicito Pizza peperoni");
+                    pizza1.setNombre("Peperoni");
+                    System.out.println("Ingrese tipo de masa:");
+                    pizza1.setMasa(teclado.next());
+                    System.out.println("Ingrese el tamano:");
+                    pizza1.setTamano(teclado.next());
+                }
+                case 3 -> {
+                    System.out.println("Solicito Pizza Hawaiana");
+                    pizza1.setNombre("Hawaiana");
+                    System.out.println("Ingrese tipo de masa:");
+                    pizza1.setMasa(teclado.next());
+                    System.out.println("Ingrese el tamano:");
+                    pizza1.setTamano(teclado.next());
+                }
+                case 4 -> {
+                    System.out.println("Cancelar pedido");
+                    pizza1.setNombre("");
+                    pizza1.setMasa("");
+                    pizza1.setTamano("");
+                }
+                case 5 -> {
+                    System.out.println("Resumen compra");
+                    System.out.println(pizza1.getNombre());
+                    System.out.println(pizza1.getMasa());
+                    System.out.println(pizza1.getTamano());
+                    pizza1.prepararPizza();
+                    pizza1.calentarPizza();
+                }
+                case 0 -> System.out.println("Salir");
             }
-        }while(opcion != 0);    
+            //case 1 -> System.out.println("Solicito Pizza napolitana");
+                    }while(opcion != 0);    
             /*
             if (opcion == 1){
                 System.out.println("Verdadero");
