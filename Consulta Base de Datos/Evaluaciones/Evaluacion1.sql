5c809ce1f00fecfc49ba9f0b0961cb37c6fca4c5
Faaian <fabi.loyola@duocuc.cl>
2023-09-09 16:01:56 -0300

    prueba añadida

--- Consulta Base de Datos/Evaluaciones/Evaluacion1.sql
+++ Consulta Base de Datos/Evaluaciones/Evaluacion1.sql
@@ -1,9 +1,16 @@
 -- Caso 1
 SELECT
-    INITCAP(pnombre_emp)||' '||SUBSTR(NVL(snombre_emp,'K'),1,1)||'. '||appaterno_emp||' '||SUBSTR(LOWER(apmaterno_emp),1,1)||'.' AS "NOMBRE EMPLEADO",
+    INITCAP(pnombre_emp)||' '
+    ||SUBSTR(NVL(snombre_emp,'K'),1,1)||'. '
+    ||appaterno_emp||' '||SUBSTR(LOWER(apmaterno_emp),1,1)
+    ||'.' AS "NOMBRE EMPLEADO",
     TO_CHAR(fecha_contrato,'FMDay d Mon YYYY')AS "FECHA CONTRATO",
     (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM fecha_contrato)) AS "AÑOS CONTRATADOS",
-    SUBSTR(UPPER(pnombre_emp),-2,2)||TRUNC(MONTHS_BETWEEN(SYSDATE,fecha_contrato))||SUBSTR(TO_CHAR(fecha_contrato,'day'),1,2)||SUBSTR(NVL(snombre_emp,'X'),1,1)||SUBSTR(numrun_emp,-2,2) AS "CONTRASEÑA"
+    SUBSTR(UPPER(pnombre_emp),-2,2)
+    ||TRUNC(MONTHS_BETWEEN(SYSDATE,fecha_contrato))
+    ||SUBSTR(TO_CHAR(fecha_contrato,'day'),1,2)
+    ||SUBSTR(NVL(snombre_emp,'X'),1,1)
+    ||SUBSTR(numrun_emp,-2,2) AS "CONTRASEÑA"
 FROM
     empleado
 WHERE
@@ -16,15 +23,19 @@ ORDER BY
 
 -- Caso 2
 SELECT
-    INITCAP(pnombre_emp)||' '||NVL(snombre_emp,'SEGUNDO')||' '||SUBSTR(appaterno_emp,1,1)||'. '||SUBSTR(apmaterno_emp,1,1) AS "NOMBRE EMPLEADO",
+    INITCAP(pnombre_emp)||' '
+    ||NVL(snombre_emp,'SEGUNDO')||' '
+    ||SUBSTR(appaterno_emp,1,1)||'. '
+    ||SUBSTR(apmaterno_emp,1,1)
+        AS "NOMBRE EMPLEADO",
     TO_CHAR(fecha_nac,'FMDay d Month yyyy')AS "FECHA NACIMIENTO",
     TO_CHAR(
-    CASE
-        WHEN id_tipo_sal = 'F' THEN (sueldo_base * 7.2)/100
-        WHEN id_tipo_sal = 'I' THEN (sueldo_base * 8.3)/100
-        WHEN id_tipo_sal = 'FA' THEN (sueldo_base * 4.5)/100
-        WHEN id_tipo_sal = 'C' THEN (sueldo_base * 5.3)/100
-        WHEN id_tipo_sal = 'P' THEN (sueldo_base * 11.3)/100
+    CASE id_tipo_sal
+        WHEN 'F' THEN (sueldo_base * 7.2)/100
+        WHEN 'I' THEN (sueldo_base * 8.3)/100
+        WHEN 'FA' THEN (sueldo_base * 4.5)/100
+        WHEN 'C' THEN (sueldo_base * 5.3)/100
+        WHEN 'P' THEN (sueldo_base * 11.3)/100
     END,'FML999g999g999') AS "DESCUENTO",
     TO_CHAR(
     CASE
@@ -32,17 +43,20 @@ SELECT
         ELSE sueldo_base * 0.15
     END,'FML999g999g999') AS "BONO",
     TO_CHAR(sueldo_base,'FML999g999g999') AS "SUELDO BASE",
-    TO_CHAR((sueldo_base + CASE
-            WHEN snombre_emp IS NULL THEN sueldo_base * 0.20
-            ELSE sueldo_base * 0.15
+    TO_CHAR((sueldo_base + 
+        CASE
+            WHEN snombre_emp IS NULL THEN (sueldo_base * 0.20)
+            ELSE (sueldo_base * 0.15)
         END) - 
-    CASE
-        WHEN id_tipo_sal = 'F' THEN (sueldo_base * 7.2)/100
-        WHEN id_tipo_sal = 'I' THEN (sueldo_base * 8.3)/100
-        WHEN id_tipo_sal = 'FA' THEN (sueldo_base * 4.5)/100
-        WHEN id_tipo_sal = 'C' THEN (sueldo_base * 5.3)/100
-        WHEN id_tipo_sal = 'P' THEN (sueldo_base * 11.3)/100
-    END,'FML999g999g999') AS "LIQUIDO"
+    CASE id_tipo_sal
+        WHEN 'F' THEN (sueldo_base * 7.2)/100
+        WHEN 'I' THEN (sueldo_base * 8.3)/100
+        WHEN 'FA' THEN (sueldo_base * 4.5)/100
+        WHEN 'C' THEN (sueldo_base * 5.3)/100
+        WHEN 'P' THEN (sueldo_base * 11.3)/100
+    END
+    ,'FML999g999g999') 
+    AS "LIQUIDO"
 FROM
     empleado
 WHERE
