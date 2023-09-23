c578bc507c991491f96c6fc1ba7f55667415b67a
Faaian <fabi.loyola@duocuc.cl>
2023-09-23 20:07:44 -0300

    a

--- Consulta Base de Datos/Todos los P/P6.sql
+++ Consulta Base de Datos/Todos los P/P6.sql
@@ -5,8 +5,9 @@ SELECT
     ,p.nombre_prof_ofic AS "PROFESION/OFICIO"
     ,TO_CHAR(c.fecha_nacimiento,'dd "de" Month') AS "DIA DE CUMPLEAÑOS"
 FROM    
-    cliente c JOIN profesion_oficio p
-    USING(cod_prof_ofic)
+    cliente c 
+    JOIN profesion_oficio p
+        USING(cod_prof_ofic)
 WHERE
     EXTRACT (MONTH FROM fecha_nacimiento) = EXTRACT(MONTH FROM SYSDATE) + 1
 ORDER BY
@@ -16,17 +17,24 @@ ORDER BY
 
 -- Caso 2
 SELECT
-    TO_CHAR(c.numrun,'FM999g999g999')||'-'||UPPER(dvrun) AS "RUN CLIENTE"
-    ,c.pnombre||' '||c.snombre||' '||appaterno||' '||apmaterno AS "NOMBRE CLIENTE"
-    ,TO_CHAR(cr.monto_solicitado,'FML999g999g999') AS "MONTO SOLICITADO CREDITOS"
-    ,TO_CHAR(cr.monto_solicitado * 1200 / 100000,'FML999g999g999') AS "TOTAL PESOS TODOSUMA"
+    TO_CHAR(c.numrun,'FM999g999g999')||'-'||UPPER(c.dvrun) AS "RUN CLIENTE"
+    ,c.pnombre||' '||c.snombre||' '||c.appaterno||' '||c.apmaterno AS "NOMBRE CLIENTE"
+    ,TO_CHAR(SUM(cc.monto_solicitado),'FML999g999g999') AS "MONTO SOLICITADO CREDITOS"
+    ,TO_CHAR(SUM(cc.monto_solicitado) * 1200 / 100000,'FML999g999g999') AS "TOTAL PESOS TODOSUMA"
 FROM
-    credito_cliente cr JOIN cliente c
-    ON(cr.nro_cliente=c.nro_cliente)
-    AND cr.total_cuotas_credito <= 36
-    AND EXTRACT(YEAR FROM cr.fecha_otorga_cred) = EXTRACT(YEAR FROM SYSDATE) - 1
+    cliente c
+    JOIN credito_cliente cc
+        ON c.nro_cliente = cc.nro_cliente
+    AND EXTRACT(YEAR FROM cc.fecha_otorga_cred) = EXTRACT(YEAR FROM SYSDATE) - 1
+GROUP BY
+    c.numrun
+    ,c.dvrun
+    ,c.pnombre
+    ,c.snombre
+    ,c.appaterno
+    ,c.apmaterno
 ORDER BY
-    cr.monto_solicitado ASC
+    SUM(cc.monto_solicitado) * 1200 / 100000
     ,c.appaterno
 ;
 
@@ -34,36 +42,74 @@ ORDER BY
 SELECT
     TO_CHAR(cc.fecha_otorga_cred,'mmyyyy') AS "MES DE TRANSCACCIÓN"
     ,c.nombre_credito AS "TIPO CREDITO"
-    ,cc.monto_solicitado AS "MONTO SOLICITADO CREDITO"
+    ,SUM(cc.monto_credito) AS "MONTO SOLICITADO CREDITO"
     ,CASE
-        WHEN cc.monto_solicitado BETWEEN 100000 AND 100000000 THEN cc.monto_credito * 0.01
-        WHEN cc.monto_solicitado BETWEEN 100000001 AND 200000000 THEN cc.monto_credito * 0.02
-        WHEN cc.monto_solicitado BETWEEN 200000001 AND 400000000 THEN cc.monto_credito * 0.03
-        WHEN cc.monto_solicitado BETWEEN 400000001 AND 600000000 THEN cc.monto_credito * 0.04
-        WHEN cc.monto_solicitado > 6000000000 THEN cc.monto_credito * 0.07
+        WHEN SUM(cc.monto_credito) BETWEEN 100000 AND 1000.000 THEN SUM(cc.monto_credito) * 0.01
+        WHEN SUM(cc.monto_credito) BETWEEN 1000001 AND 2000000 THEN SUM(cc.monto_credito) * 0.02
+        WHEN SUM(cc.monto_credito) BETWEEN 2000001 AND 4000000 THEN SUM(cc.monto_credito) * 0.03
+        WHEN SUM(cc.monto_credito) BETWEEN 4000001 AND 6000000 THEN SUM(cc.monto_solicitado) * 0.04
+        WHEN SUM(cc.monto_credito) > 6000000 THEN SUM(cc.monto_credito) * 0.07
     END
         AS "APORTE A LA SBIF"
 FROM
     credito_cliente cc 
-    INNER JOIN credito c
-    ON cc.cod_credito = c.cod_credito
+        JOIN credito c
+            ON cc.cod_credito = c.cod_credito
 WHERE
     EXTRACT(YEAR FROM cc.fecha_otorga_cred) = EXTRACT(YEAR FROM SYSDATE) - 1
+GROUP BY
+    TO_CHAR(cc.fecha_otorga_cred,'mmyyyy')
+    ,c.nombre_credito
 ORDER BY
     "MES DE TRANSCACCIÓN" ASC
     ,c.nombre_credito
 ;
-
+ 
 -- Caso 4
 SELECT
     TO_CHAR(c.numrun,'FM999g999g999')||'-'||c.dvrun AS "RUN CLIENTE"
     ,c.pnombre||' '||c.snombre||' '||c.appaterno||' '||c.apmaterno AS "NOMBRE CLIENTE"
-    ,TO_CHAR(p.monto_total_ahorrado,'FML999g999g999') AS "MONTO TOTAL AHORRADO"
+    ,TO_CHAR(SUM(p.monto_total_ahorrado),'FML999g999g999') AS "MONTO TOTAL AHORRADO"
+    ,CASE
+        WHEN SUM(p.monto_total_ahorrado) BETWEEN 100000 AND 1000000 THEN 'BRONCE'
+        WHEN SUM(p.monto_total_ahorrado) BETWEEN 1000001 AND 4000000 THEN 'PLATA'
+        WHEN SUM(p.monto_total_ahorrado) BETWEEN 4000001 AND 8000000 THEN 'SILVER'
+        WHEN SUM(p.monto_total_ahorrado) BETWEEN 8000001 AND 15000000 THEN 'GOLD'
+        WHEN SUM(p.monto_total_ahorrado) > 15000000 THEN 'PLATINUM'
+    END
+        AS "CATEGORIA CLIENTE"
 FROM
     producto_inversion_cliente p
-RIGHT JOIN cliente c
-ON p.nro_cliente = c.nro_cliente
+        JOIN cliente c
+            ON p.nro_cliente = c.nro_cliente
+GROUP BY
+    c.numrun, c.dvrun, 
+    c.pnombre, c.snombre, 
+    c.appaterno, c.apmaterno
 ORDER BY
     c.appaterno
-    ,"MONTO TOTAL AHORRADO" ASC
+    ,SUM(p.monto_total_ahorrado) ASC
+;
+
+-- Caso 5
+SELECT
+    TO_CHAR(SYSDATE,'YYYY') AS "AÑO TRIBUTARIO"
+    ,TO_CHAR(c.numrun,'FM00g999g999')||'-'||c.dvrun AS "RUN CLIENTE"
+    ,c.pnombre||' '||SUBSTR(c.snombre,1,1)||'. '||c.appaterno||' '||c.apmaterno AS "NOMBRE COMPLETO"
+    ,COUNT(*) AS "TOTAL PROD. INV AFECTOS IMPTO"
+    ,TO_CHAR(SUM(p.monto_total_ahorrado),'L999g999g999') AS "MONTO TOTAL AHORRADO"
+FROM
+    cliente c
+        JOIN producto_inversion_cliente p
+            ON c.nro_cliente = p.nro_cliente
+GROUP BY
+    c.numrun, c.dvrun, 
+    c.pnombre, c.snombre, 
+    c.appaterno, c.apmaterno,
+    p.fecha_solic_prod
+    HAVING COUNT(*) > 1
+OR
+    SUM(p.monto_total_ahorrado) > 7833186 
+ORDER BY
+   c.appaterno 
 ;
