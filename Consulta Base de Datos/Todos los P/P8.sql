d104b808705261f7b2b0e351c5473530d1512ec8
Faaian <fabi.loyola@duocuc.cl>
2023-10-19 04:16:26 -0300

    p8

--- Consulta Base de Datos/Todos los P/P8.sql
+++ Consulta Base de Datos/Todos los P/P8.sql
@@ -1,26 +1,27 @@
 SELECT
-    descripcion_tp||','||descripcion_sa AS "SISTEMA_SALUD"
-    ,total AS "TOTAL ATENCIONES"
+    nombre_tipo||','||nombre_sal AS "SISTEMA_SALUD"
+    ,cantidad AS "TOTAL ATENCIONES"
 FROM
-    (SELECT COUNT(at.ate_id) AS total
-    ,sa.descripcion AS descripcion_sa
-    ,tp.descripcion AS descripcion_tp
-FROM atencion at
-    JOIN paciente p 
+    (SELECT
+    sa.descripcion AS nombre_sal
+    ,tp.descripcion AS nombre_tipo
+    ,COUNT(at.ate_id) AS cantidad
+FROM
+    atencion at JOIN paciente p
         ON at.pac_run = p.pac_run
-    JOIN salud sa 
-        ON p.sal_id = sa.sal_id
-    JOIN tipo_salud tp 
-        ON sa.tipo_sal_id = tp.tipo_sal_id
-WHERE 
-    EXTRACT(MONTH FROM at.fecha_atencion) = EXTRACT(MONTH FROM SYSDATE)
-GROUP BY 
-    tp.descripcion, sa.descripcion)
+    JOIN salud sa ON p.sal_id = sa.sal_id
+    JOIN tipo_salud tp ON sa.tipo_sal_id = tp.tipo_sal_id
 WHERE
-    total > ANY
-    (SELECT COUNT(at.ate_id) AS atenciones
-    FROM atencion at
-    WHERE TO_CHAR(at.fecha_atencion,'mm') = '09'
-    GROUP BY TO_CHAR(at.fecha_atencion,'dd'))
+    EXTRACT(MONTH FROM at.fecha_atencion) = EXTRACT(MONTH FROM SYSDATE)
+GROUP BY
+    sa.descripcion, tp.descripcion)
+WHERE
+    cantidad > 
+        (SELECT
+            ROUND(AVG(atenciones))
+        FROM
+        (SELECT COUNT(at.ate_id) AS atenciones
+        FROM atencion at
+        WHERE TO_CHAR(at.fecha_atencion,'mm') = '09'
+        GROUP BY TO_CHAR(at.fecha_atencion,'dd')))
 ;
--------------
