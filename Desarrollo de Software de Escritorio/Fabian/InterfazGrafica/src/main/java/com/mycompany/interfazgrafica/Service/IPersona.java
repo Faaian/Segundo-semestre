7b15ed159c7f2571771fcf407d324c3ff8f0b92f
Your Name <you@example.com>
2023-11-16 22:03:16 -0300

    avanzado

---
+++ Desarrollo de Software de Escritorio/Fabian/InterfazGrafica/src/main/java/com/mycompany/interfazgrafica/Service/IPersona.java
@@ -0,0 +1,12 @@
+package com.mycompany.interfazgrafica.Service;
+
+import com.mycompany.interfazgrafica.DTO.PersonaDTO;
+
+public interface IPersona {
+    
+    public void Guardar(PersonaDTO p);
+    
+    
+    
+    public int contador();
+}
